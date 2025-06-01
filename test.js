8321ee58f71f740df4a50ca6237b67096c45ed4e
dhrubb <dhrubbgupta@gmail.com>
2025-06-01 20:45:51 +0530

    project structure files

---
+++ test.js
@@ -0,0 +1,16 @@
+console.log("This is a test file to check if the code is running correctly.");
+
+//bun,dino,node are execution engines for javascript
+// bun is a fast JavaScript runtime like Node.js, but with a focus on performance.
+// dino is a secure JavaScript and TypeScript runtime built on V8, focusing on developer experience and productivity.
+// node is a popular JavaScript runtime built on Chrome's V8 engine, widely used for building server-side applications. 
+
+//then we use npm init to initialize a new Node.js projectthat means initializing a new project with node.js environment
+
+//then we give version of the project in three parts major.minor.patch
+//major is the first part of the version number, minor is the second part, and patch is the third part.
+// we get it from Semver.org
+
+//then to use third party libraries  and the code within and them and use it we use require and import
+//today we use import instead of require
+//and if we are using import then we need to use type module in package.json file and change it to "module "
